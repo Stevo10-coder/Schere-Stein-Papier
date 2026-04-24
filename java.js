a2a1df27aca5501664ff53deea8532f23f92baad
Stevo10-Coder <202168287+Stevo10-coder@users.noreply.github.com>
2026-04-24 21:35:41 +0200

    function  playGame created

--- java.js
+++ java.js
@@ -5,19 +5,44 @@ function getComputerChoice() {
     return wordList[randomIndex];
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
-    const question = prompt("Schere, Stein oder Papier?");
-    return question
+    const question = prompt("Wähle Schere, Stein oder Papier?");
+    return question[0].toUpperCase() + question.slice(1).toLowerCase();
    
 }
-
-console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
 
+    computerChoice === computerChoice.toLowerCase();
+
+    if (humanChoice === computerChoice){
+        console.log("Unentschieden!");
+    } else if (humanChoice === "Papier" && computerChoice === "Stein") {
+        console.log("Papier schlägt Stein! Du hast gewonnen!");
+        humanScore++;
+        console.log("HumanScore:", + humanScore);
+    } else if (humanChoice === "Stein" && computerChoice === "Schere") {
+        console.log("Stein schlägt Schere! Du hast gewonnen!");
+        humanScore++;
+        console.log("HumanScore:", + humanScore);
+    } else if (humanChoice === "Schere" && computerChoice === "Papier") {
+        console.log("Schere schlägt Papier! Du hast gewonnen!");
+        humanScore++;
+        console.log("HumanScore:", + humanScore);
+    } else {
+        console.log("Du hast verloren!");
+        computerScore++;
+        console.log("ComputerScore:", + computerScore);
+    }
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("Du:", humanSelection);
+console.log("Computer:", computerSelection);
+
+playRound(humanSelection, computerSelection);
