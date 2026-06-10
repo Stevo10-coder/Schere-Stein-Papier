d4b70b440afcfd52dddcfe8b34c56aad6ca22d57
Stevo10-Coder <202168287+Stevo10-coder@users.noreply.github.com>
2026-06-10 21:14:42 +0200

    Add DOM controls and score display to Rock Paper Scissors

--- java.js
+++ java.js
@@ -1,64 +1,85 @@
 
 
 const wordList = ["Schere", "Stein", "Papier"];
+const schereBtn = document.querySelector("#Schere");
+const steinBtn = document.querySelector("#Stein");
+const papierBtn = document.querySelector("#Papier");
+const results = document.querySelector("#results");
+const score = document.querySelector("#score");
+const newButton = document.querySelector("#newButton");
+
+
+
+let humanScore = 0;
+    let computerScore = 0;
+
+results.textContent = "Viel Glück beim Spielen :)";
+
 
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * wordList.length);
     return wordList[randomIndex];
 }
 
-function getHumanChoice() {
-    const question = prompt("Wähle Schere, Stein oder Papier?");
-    return question[0].toUpperCase() + question.slice(1).toLowerCase();
-   
+// Button für das Spiel Geschehen
+schereBtn.addEventListener("click", function() {
+    playRound("Schere", getComputerChoice());
+});
+
+steinBtn.addEventListener("click", function() {
+    playRound("Stein", getComputerChoice());
+});
+
+papierBtn.addEventListener("click", function() {
+    playRound("Papier", getComputerChoice());
+});
+
+newButton.addEventListener("click", function() {
+    resetGame();
+});
+
+function resetGame () {
+    humanScore = 0;
+    computerScore = 0;
+
+    results.textContent = "Viel Glück beim spielen! :D";
+    score.textContent = "Score - Du: 0  ||  Computer: 0";
+    console.log(humanScore, computerScore);
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+
 
 function playRound(humanChoice, computerChoice) {
+    let roundMessage = "";
 
-    if (humanChoice === computerChoice){
-        console.log("Unentschieden!");
-    } else if (humanChoice === "Papier" && computerChoice === "Stein") {
-        console.log("Papier schlägt Stein! Du hast gewonnen!");
-        humanScore++;
-        console.log("HumanScore:", + humanScore);
-    } else if (humanChoice === "Stein" && computerChoice === "Schere") {
-        console.log("Stein schlägt Schere! Du hast gewonnen!");
-        humanScore++;
-        console.log("HumanScore:", + humanScore);
-    } else if (humanChoice === "Schere" && computerChoice === "Papier") {
-        console.log("Schere schlägt Papier! Du hast gewonnen!");
-        humanScore++;
-        console.log("HumanScore:", + humanScore);
-    } else {
-        console.log("Du hast verloren!");
-        computerScore++;
-        console.log("ComputerScore:", + computerScore);
+    // Beendet das Spiel wenn jemand 5 Punkte hat.
+    if (humanScore === 5 || computerScore === 5) {
+        return;
     }
 
-    console.log(`Score: Du ${humanScore} - ${computerScore} Computer`);
+    if (humanChoice === computerChoice) {
+        roundMessage = `Unentschieden! - Beide haben ${humanChoice}`;
+    } else if (
+        (humanChoice === "Stein" && computerChoice === "Schere" || 
+         humanChoice === "Schere" && computerChoice === "Papier" ||
+         humanChoice === "Papier" && computerChoice === "Stein")  
+        ) {
+            roundMessage = `${humanChoice} schlägt ${computerChoice}! - Du gewinnst diese Runde!`;
+            humanScore++
+        } else {
+            roundMessage = `${computerChoice} schlägt ${humanChoice}! - Der Computer gewinnt diese Runde!`
+            computerScore++
+        }
+
+
+    results.textContent = `${roundMessage}`;
+    score.textContent = `Score - Du: ${humanScore} || Computer: ${computerScore}`;
+
+
+  if (humanScore === 5) {
+    results.textContent = "Du hast das Spiel gewonnen!";
+  } else if (computerScore === 5) {
+    results.textContent = "Der Computer hat das Spiel gewonnen!"
+  }
+
 }
-
-
-playRound(getHumanChoice(), getComputerChoice());
-playRound(getHumanChoice(), getComputerChoice());
-playRound(getHumanChoice(), getComputerChoice());
-playRound(getHumanChoice(), getComputerChoice());
-playRound(getHumanChoice(), getComputerChoice());
-
-console.log(" -- Spiel beendet --");
-
-if (humanScore > computerScore) {
-    console.log("Glückwünsch du hast gewonnen!");
-} else if (humanScore < computerScore) {
-    console.log("Computer hat gewonnen!!");
-} else  {
-    console.log("Leider Unentschieden!");
-}
-
-}
-
-playGame();
